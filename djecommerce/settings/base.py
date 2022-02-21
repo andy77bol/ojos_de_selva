6ebe7fe1920f56137b411189cf49c967e1caed26
Andreas Reisinger <andreas.reisinger97@gmail.com>
2022-02-20 20:05:25 -0400

    Another change trying to make deployment on DigitalOcean possible

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -98,7 +98,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
-STATIC_URL = '/static_in_env/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
